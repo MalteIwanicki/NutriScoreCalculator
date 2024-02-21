1b316d9c5345b4bcd432c4cbbd40ceb6fa911ddf
Malte Iwanicki <45853662+MalteIwanicki@users.noreply.github.com>
2024-02-21 01:35:36 +0100

    Update setup.py
    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nutri_score_calculator',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     url='https://github.com/malteiwanicki/nutri_score_calculator',
     license='MIT',
